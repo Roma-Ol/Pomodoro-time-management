b1c695393bfd9f55a79aa21406cb8c9f46cc6581
RomaOl <olievskiyroman@gmail.com>
2022-12-13 17:32:47 +0200

    Added file structure and basic logic

--- src/components/InputGroup/InputGroup.tsx
+++ src/components/InputGroup/InputGroup.tsx
@@ -1,15 +1,33 @@
-const InputGroup = () => {
+import {FC, useRef} from "react";
+import {InputGroupProps} from "../../../types";
+
+const InputGroup: FC<InputGroupProps> = ({
+    isRunning,
+    setTimeSetting,
+}) => {
+    const timeRef = useRef(null);
+    const pauseDurRef = useRef(null);
+    const cycleCountRef = useRef(null);
+
+    function operateTime() {
+        const timeSetting = {
+            time: timeRef.current.value === '' ? 0 : +timeRef.current.value,
+            pauseDuration: pauseDurRef.current.value === '' ? 0 : +pauseDurRef.current.value,
+            cycleCount: cycleCountRef.current.value === '' ? 0 : +cycleCountRef.current.value,
+        }
+
+        setTimeSetting(timeSetting);
+    }
+
     return (
         <div className={'inputGroup'}>
             <p>Time to keep focus</p>
-            <input type="number"/>
+            <input onKeyUp={operateTime} type="number" ref={timeRef} placeholder={'minutes'}/>
             <p>Pause duration</p>
-            <input type="number"/>
+            <input onKeyUp={operateTime} type="number" ref={pauseDurRef} placeholder={'minutes'}/>
             <p>Cycles</p>
-            <input type="number"/>
-            <button>Start</button>
+            <input onKeyUp={operateTime} type="number" ref={cycleCountRef} placeholder={'minutes'}/>
         </div>
-
     )
 }
 
--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,6 +1,22 @@
-const Timer = () => {
-    return(
-        <h2>00:00:00</h2>
+import {FC} from "react";
+import {TimeSettingObject} from "../../../types";
+
+const Timer: FC<TimeSettingObject> = ({timeData, isRunning, start}) => {
+    const {hours, minutes, seconds} = timeData;
+
+    return (
+        <>
+            <h3>Timer</h3>
+            {isRunning
+                ? <>
+                    <div>{hours}h</div>
+                    <div>{minutes}m</div>
+                    <div>{seconds}s</div>
+                </>
+                : <>Not running</>
+            }
+            <button onClick={start}>Start</button>
+        </>
     )
 }
 
